a695c7f5c43aa5f94ad5546227766adca8197bef
bfeng <none@none>
2011-09-21 17:59:40 -0500

    Renamed CronParse to Cronscription.
    --HG--
    rename : cron_parse_spec.rb => cronscription_spec.rb

--- cronscription_spec.rb
+++ cronscription_spec.rb
@@ -1,14 +1,4 @@
-class CronParse
-  def initialize(crontab_lines)
-    # Eliminate all lines starting with '#' since they are full comments
-    @entries = crontab_lines.select{|l| l !~ /^\s*#/}.map{|e| Entry.new(e)}
-  end
-
-  def find(regex)
-    @entries.select{|e| e.match_command?(regex)}
-  end
-
-
+module Cronscription
   class Entry
     ORDERED_KEYS = [:min, :hour, :day, :month, :wday]
     FULL_RANGE = {
@@ -18,6 +8,7 @@ class CronParse
       :month => (1..12).to_a,
       :wday  => (0..6).to_a,
     }
+
     attr_reader :times, :command
 
     def initialize(line)
@@ -51,64 +42,51 @@ class CronParse
     end
 
     def times_to_execute_between(start, finish)
-      current = start
       ret = []
+
+      incr_min  = 60
+      incr_hour = incr_min*60
+      incr_day  = incr_hour*24
+      incr      = incr_min
+
+      current = start
       while current < finish
-        ret << current if ORDERED_KEYS.map{|k| times[k].include?(current.send k)}.all?
-        current += 60 # increment by a minute
+        if ORDERED_KEYS.map{|k| @times[k].include?(current.send k)}.all?
+          ret << current 
+          # If only I could goto into the middle of the loop, this wouldn't run every time.
+          # Optimizations to reduce execution time.  No need to run minutely if there is only one minute.
+          if @times[:min].size == 1
+            if @times[:hour].size == 1
+              incr = incr_day
+            else
+              incr = incr_hour
+            end
+          end
+        end
+        current += incr
       end
+
+      ret
+    end
+  end
+
+  class Tab
+    def initialize(crontab_lines)
+      # Eliminate all lines starting with '#' since they are full comments
+      @entries = crontab_lines.select{|l| l !~ /^\s*#/}.map{|e| Entry.new(e)}
+    end
+
+    def find(regex)
+      @entries.select{|e| e.match_command?(regex)}
     end
   end
 end
 
 
-
-describe CronParse do
-  before :all do
-    @crontab = <<-END
-      # <minute> <hour> <day> <month> <day of week> <tags and command>
-      0  *  *  *  *   cron.hourly
-      0  0  *  *  *   cron.daily
-      0  0  *  *  0   cron.weekly
-      0  0  1  *  *   cron.monthly
-      1  2  3  4  5   0 # test trailing comment
-    END
-    @crontab_lines = @crontab.lines.to_a
-    @cron_parse = CronParse.new(@crontab_lines)
-  end
-
-  describe 'find' do
-    it 'should find the daily entry' do
-      entries = @cron_parse.find(/daily/).map{|e| e.to_s}
-      entries.should == [@crontab_lines[2]]
-    end
-
-    it 'should find all cron.* entries' do
-      entries = @cron_parse.find(/cron\..*/).map{|e| e.to_s}
-      entries.should == @crontab_lines[1..4]
-    end
-
-    it 'should ignore complete line comments' do
-      entries = @cron_parse.find(/.*/).map{|e| e.to_s}
-      entries.should == @crontab_lines[1..-1]
-    end
-
-    it 'should ignore trailing comments' do
-      entries = @cron_parse.find(/test trailing comment/).map{|e| e.to_s}
-      entries.should == []
-    end
-
-    it 'should ignore time directives' do
-      entries = @cron_parse.find(/0/).map{|e| e.to_s}
-      entries.should == [@crontab_lines[-1]]
-    end
-  end
-end
-
-describe CronParse::Entry do
+describe Cronscription::Entry do
   describe 'parse_column' do
     before :all do
-      @entry = CronParse::Entry.new('1 2 3 4 5 6')
+      @entry = Cronscription::Entry.new('1 2 3 4 5 6')
     end
 
     it 'should return fixed value as list of one' do
@@ -140,51 +118,49 @@ describe CronParse::Entry do
   describe 'from_s' do
     it 'should map columns with correct fields' do
       # <minute> <hour> <day> <month> <day of week> <tags and command>
-      entry = CronParse::Entry.new('1 2 3 4 5 comm')
-      entry.times[:min].should   == [1]
-      entry.times[:hour].should  == [2]
-      entry.times[:day].should   == [3]
-      entry.times[:month].should == [4]
-      entry.times[:wday].should  == [5]
-      entry.command.should       == 'comm'
+      entry = Cronscription::Entry.new('1 2 3 4 5 comm')
+      entry.times.should == {
+        :min => [1],
+        :hour => [2],
+        :day => [3],
+        :month => [4],
+        :wday => [5],
+      }
+      entry.command.should == 'comm'
     end
 
     it 'should convert to original line on to_s' do
       line = '      1 2 3 4 5 fun today! # some comment'
-      entry = CronParse::Entry.new(line)
+      entry = Cronscription::Entry.new(line)
       entry.to_s.should == line
     end
 
     it 'should use FULL_RANGE for default values' do
-      entry = CronParse::Entry.new('* * * * * comm')
-      entry.times[:min].should   == CronParse::Entry::FULL_RANGE[:min]
-      entry.times[:hour].should  == CronParse::Entry::FULL_RANGE[:hour]
-      entry.times[:day].should   == CronParse::Entry::FULL_RANGE[:day]
-      entry.times[:month].should == CronParse::Entry::FULL_RANGE[:month]
-      entry.times[:wday].should  == CronParse::Entry::FULL_RANGE[:wday]
+      entry = Cronscription::Entry.new('* * * * * comm')
+      entry.times.should == Cronscription::Entry::FULL_RANGE
     end
   end
 
   describe 'match_command?' do
     it 'should match command by regex' do
-      entry = CronParse::Entry.new('1 2 3 4 5 command one')
+      entry = Cronscription::Entry.new('1 2 3 4 5 command one')
       entry.match_command?(/m*and\s*on/).should be_true
     end
 
     it 'should not match command within comments' do
-      entry = CronParse::Entry.new('1 2 3 4 5 command #herp')
+      entry = Cronscription::Entry.new('1 2 3 4 5 command #herp')
       entry.match_command?(/herp/).should be_false
     end
 
     it 'should not match command within time directives' do
-      entry = CronParse::Entry.new('1 2 3 4 5 command #herp')
+      entry = Cronscription::Entry.new('1 2 3 4 5 command #herp')
       entry.match_command?(/\d/).should be_false
     end
   end
 
   describe 'times_to_execute_between' do
     it 'should return times based on minutes' do
-      entry = CronParse::Entry.new("21-40 * * * * comm")
+      entry = Cronscription::Entry.new("21-40 * * * * comm")
       start = Time.local(2011, 1, 1, 0, 0)
       finish = Time.local(2011, 1, 1, 0, 30)
 
@@ -193,7 +169,7 @@ describe CronParse::Entry do
     end
 
     it 'should return times based on hours' do
-      entry = CronParse::Entry.new("0 1-8 * * * comm")
+      entry = Cronscription::Entry.new("0 1-8 * * * comm")
       start = Time.local(2011, 1, 1, 0, 0)
       finish = Time.local(2011, 1, 1, 6, 0)
 
@@ -202,7 +178,7 @@ describe CronParse::Entry do
     end
 
     it 'should return times based on days' do
-      entry = CronParse::Entry.new("0 0 8-20 * * comm")
+      entry = Cronscription::Entry.new("0 0 8-20 * * comm")
       start = Time.local(2011, 1, 5, 0, 0)
       finish = Time.local(2011, 1, 15, 0, 0)
 
@@ -211,7 +187,7 @@ describe CronParse::Entry do
     end
 
     it 'should return times based on months' do
-      entry = CronParse::Entry.new("0 0 1 1-5 * comm")
+      entry = Cronscription::Entry.new("0 0 1 1-5 * comm")
       start = Time.local(2011, 4, 1, 0, 0)
       finish = Time.local(2011, 12, 1, 0, 0)
 
@@ -220,3 +196,51 @@ describe CronParse::Entry do
     end
   end
 end
+
+describe Cronscription::Tab do
+  before :all do
+    @crontab = <<-END
+      # <minute> <hour> <day> <month> <day of week> <tags and command>
+      0  *  *  *  *   cron.hourly
+      0  0  *  *  *   cron.daily
+      0  0  *  *  0   cron.weekly
+      0  0  1  *  *   cron.monthly
+      1  2  3  4  5   0 # test trailing comment
+    END
+    @crontab_lines = @crontab.lines.to_a
+    @tab = Cronscription::Tab.new(@crontab_lines)
+  end
+
+  describe 'find' do
+    it 'should find the daily entry' do
+      entries = @tab.find(/daily/).map{|e| e.to_s}
+      entries.should == [@crontab_lines[2]]
+    end
+
+    it 'should find all cron.* entries' do
+      entries = @tab.find(/cron\..*/).map{|e| e.to_s}
+      entries.should == @crontab_lines[1..4]
+    end
+
+    it 'should ignore complete line comments' do
+      entries = @tab.find(/.*/).map{|e| e.to_s}
+      entries.should == @crontab_lines[1..-1]
+    end
+
+    it 'should ignore trailing comments' do
+      entries = @tab.find(/test trailing comment/).map{|e| e.to_s}
+      entries.should == []
+    end
+
+    it 'should ignore time directives' do
+      entries = @tab.find(/0/).map{|e| e.to_s}
+      entries.should == [@crontab_lines[-1]]
+    end
+  end
+
+  describe 'merged_times' do
+    it 'should merge times into sorted array' do
+    end
+  end
+end
+
